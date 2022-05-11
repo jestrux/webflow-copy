c1bbc3dfab28f7118dbed505f815997caae5b6c6
Walter Kimaro <wakyj07@gmail.com>
2022-05-11 15:04:11 +0300

    Initial setup

---
+++ components/red-div.js
@@ -0,0 +1,51 @@
+export default {
+    "type": "@webflow/XscpData",
+    "payload": {
+        "nodes": [
+            {
+                "_id": "a67cb4c6-5455-6d9b-6222-2b2e6c20b40a",
+                "tag": "div",
+                "classes": [
+                    "d966f934-bbfc-678c-3789-45f0a8782d01"
+                ],
+                "children": [],
+                "type": "Section",
+                "data": {
+                    "tag": "div",
+                    "grid": {
+                        "type": "section"
+                    }
+                }
+            }
+        ],
+        "styles": [
+            {
+                "_id": "d966f934-bbfc-678c-3789-45f0a8782d01",
+                "fake": false,
+                "type": "class",
+                "name": "bg-section",
+                "namespace": "",
+                "comb": "",
+                "styleLess": "height: 400px; background-color: yellow;",
+                "variants": {},
+                "children": [],
+                "createdBy": "5eea119b79c2885ecfd3b301",
+                "selector": null
+            }
+        ],
+        "assets": [],
+        "ix1": [],
+        "ix2": {
+            "interactions": [],
+            "events": [],
+            "actionLists": []
+        }
+    },
+    "meta": {
+        "unlinkedSymbolCount": 0,
+        "droppedLinks": 0,
+        "dynBindRemovedCount": 0,
+        "dynListBindRemovedCount": 0,
+        "paginationRemovedCount": 0
+    }
+}
